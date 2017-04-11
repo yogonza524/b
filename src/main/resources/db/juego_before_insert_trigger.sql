b2d96ba89231ce07a76d30e5aba18e2775b788eb
Gonzalo Mendoza <yogonza524@gmail.com>
2017-04-11 11:06:40 -0300

    Agregando el trigger a la tabla juego

---
+++ src/main/resources/db/juego_before_insert_trigger.sql
@@ -0,0 +1,62 @@
+﻿CREATE OR REPLACE FUNCTION before_update_juego()
+RETURNS TRIGGER AS $$
+DECLARE
+
+BEGIN 
+	NEW.id = 0;
+	IF(NEW.creditos IS NULL) THEN
+		NEW.creditos = 0;
+	END IF;
+
+	IF(NEW.apuesta_total IS NULL) THEN
+		NEW.apesta_total = 1;
+		NEW.carton1_habilitado = true;
+		NEW.carton2_habilitado = true;
+		NEW.carton3_habilitado = true;
+		NEW.carton4_habilitado = true;
+	END IF;
+
+	IF(NEW.denominacion_actual IS NULL) THEN
+		NEW.denominacion_actual = 'CINCO_CENTAVOS';
+		NEW.denominacion_factor = 20;
+	END IF;
+
+	IF(NEW.denominacion_factor IS NULL) THEN
+		NEW.denominacion_actual = 'CINCO_CENTAVOS';
+		NEW.denominacion_factor = 20;
+	END IF;
+
+	IF (NEW.acumulado is NULL) THEN
+		NEW.acumulado = 0.0;
+	END IF;
+
+	IF(NEW.carton1_habilitado IS NULL) THEN
+		NEW.carton1_habilitado = true;
+	END IF;
+
+	IF(NEW.carton2_habilitado IS NULL) THEN
+		NEW.carton2_habilitado = false;
+	END IF;
+
+	IF(NEW.carton3_habilitado IS NULL) THEN
+		NEW.carton3_habilitado = false;
+	END IF;
+
+	IF(NEW.carton4_habilitado IS NULL) THEN
+		NEW.carton4_habilitado = false;
+	END IF;
+
+	IF(NEW.denominacion_factor = 10) THEN 
+		NEW.denominacion_actual = 'DIEZ_CENTAVOS';
+	END IF;
+
+	IF(NEW.denominacion_factor = 20) THEN 
+		NEW.denominacion_actual = 'CINCO_CENTAVOS';
+	END IF;
+
+	NEW.dinero = NEW.creditos / NEW.denominacion_factor;
+
+	RETURN NEW;
+END;
+$$
+LANGUAGE PLPGSQL
